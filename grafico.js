152d6b8f2087d41d7ae85e5b75a7454abf973fa8
gravagabriel <64667924+gravagabriel@users.noreply.github.com>
2024-10-07 11:50:22 -0300

    Add files via upload

---
+++ grafico.js
@@ -0,0 +1,9 @@
+// Gráfico de Pizza
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new CharacterData(ctxPizza, {
+    type 'pie',
+    data: {
+        labels: [],
+    }
+}
+    )
