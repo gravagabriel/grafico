12d331d1e24d4feb0d90d5954ac2c1a9663ff042
gravagabriel <64667924+gravagabriel@users.noreply.github.com>
2024-10-11 09:53:36 -0300

    Add files via upload

--- grafico.js
+++ grafico.js
@@ -4,6 +4,27 @@ const graficoPizza = new CharacterData(ctxPizza, {
     type 'pie',
     data: {
         labels: [],
+        datasets: [{
+            label: 'Cursos & vontades',
+            data: [45,30,15,10],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderColor: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        Plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            tittle: {
+                display: true,
+                text: 'Participa'
+            }
+        }
     }
 }
     )
