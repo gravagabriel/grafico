2913c1e7ef227d944715ff64c47827c5a59a7b90
gravagabriel <64667924+gravagabriel@users.noreply.github.com>
2024-10-16 08:14:58 -0300

    Add files via upload

--- grafico.js
+++ grafico.js
@@ -1,9 +1,9 @@
 // Gráfico de Pizza
 const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
 const graficoPizza = new CharacterData(ctxPizza, {
-    type 'pie',
+    type: 'pie',
     data: {
-        labels: [],
+        labels: ['sla'],
         datasets: [{
             label: 'Cursos & vontades',
             data: [45,30,15,10],
@@ -26,5 +26,37 @@ const graficoPizza = new CharacterData(ctxPizza, {
             }
         }
     }
-}
-    )
+});
+
+// Gráfico de Barras
+const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
+const graficoBarra = new CharacterData(ctxBarra,{
+    type: 'bar',
+    data: {
+        labels: [   ],
+        datasets: [{
+            label: 'Usuários que responderam',
+            data: [2800, 1500, 330, 810],
+            backgroundColor: '#F05454',
+            borderColor: '#30475E',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Isso ae mano'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
